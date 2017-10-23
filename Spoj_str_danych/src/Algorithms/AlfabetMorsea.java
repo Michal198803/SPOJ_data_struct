9a87c87072802a9fdb82b95d12b263aa9d7d4c1e
Michał_progr <Micha_progr@192.168.0.2>
2017-10-23 22:47:11 +0200

    Second commit

--- Spoj_str_danych/src/Algorithms/AlfabetMorsea.java
+++ Spoj_str_danych/src/Algorithms/AlfabetMorsea.java
@@ -1,67 +1,87 @@
 package Algorithms;
 
 import java.util.HashMap;
+import java.util.Scanner;
 
 public class AlfabetMorsea {
+	
+	
+	
+	
+	
 
 	public static void main(String[] args) {
-		HashMap<String, String> newMap = new HashMap<String, String>();
-		newMap.put("A", ".-");
-		newMap.put("a", ".-");
-		newMap.put("B", "-...");
-		newMap.put("b", "-...");
-		newMap.put("C", "-.-.");
-		newMap.put("c", "-.-.");
-		newMap.put("D", "-..");
-		newMap.put("d", "-..");
-		newMap.put("E", ".");
-		newMap.put("e", ".");
-		newMap.put("F", ".-..");
-		newMap.put("f", ".-..");
-		newMap.put("G", "--.");
-		newMap.put("g", "--.");
-		newMap.put("H", "....");
-		newMap.put("h", "....");
-		newMap.put("I", "..");
-		newMap.put("i", "..");
-		newMap.put("J", ".---");
-		newMap.put("j", ".---");
-		newMap.put("K", "-.-");
-		newMap.put("k", "-.-");
-		newMap.put("L", "..-.");
-		newMap.put("l", "..-.");
-		newMap.put("M", "--");
-		newMap.put("m", "--");
-		newMap.put("N", "-.");
-		newMap.put("n", "-.");
-		newMap.put("O", "---");
-		newMap.put("o", "---");
-		newMap.put("P", ".--.");
-		newMap.put("p", ".--.");
-		newMap.put("Q", "--.-");
-		newMap.put("q", "--.-");
-		newMap.put("R", ".-.");
-		newMap.put("r", ".-.");
-		newMap.put("S", "...");
-		newMap.put("s", "...");
-		newMap.put("T", "-");
-		newMap.put("t", "-");
-		newMap.put("U", "..-");
-		newMap.put("u", "..-");
-		newMap.put("V", "...-");
-		newMap.put("v", "...-");
-		newMap.put("W", ".--");
-		newMap.put("w", ".--");
-		newMap.put("X", "-..-");
-		newMap.put("x", "-..-");
-		newMap.put("Y", "-.--");
-		newMap.put("y", "-.--");
-		newMap.put("Z", "--..");
-		newMap.put("z", "--..");
+		HashMap<Character, String> newMap = new HashMap<Character, String>();
+		newMap.put('A', ".-/");
+		newMap.put('a', ".-/");
+		newMap.put('B', "-.../");
+		newMap.put('b', "-.../");
+		newMap.put('C', "-.-./");
+		newMap.put('c', "-.-./");
+		newMap.put('D', "-../");
+		newMap.put('d', "-../");
+		newMap.put('E', "./");
+		newMap.put('e', "./");
+		newMap.put('F', ".-../");
+		newMap.put('f', ".-../");
+		newMap.put('G', "--./");
+		newMap.put('g', "--./");
+		newMap.put('H', "..../");
+		newMap.put('h', "..../");
+		newMap.put('I', "../");
+		newMap.put('i', "../");
+		newMap.put('J', ".---/");
+		newMap.put('j', ".---/");
+		newMap.put('K', "-.-/");
+		newMap.put('k', "-.-/");
+		newMap.put('L', "..-./");
+		newMap.put('l', "..-./");
+		newMap.put('M', "--/");
+		newMap.put('m', "--/");
+		newMap.put('N', "-./");
+		newMap.put('n', "-./");
+		newMap.put('O', "---/");
+		newMap.put('o', "---/");
+		newMap.put('P', ".--./");
+		newMap.put('p', ".--./");
+		newMap.put('Q', "--.-/");
+		newMap.put('q', "--.-/");
+		newMap.put('R', ".-./");
+		newMap.put('r', ".-./");
+		newMap.put('S', ".../");
+		newMap.put('s', ".../");
+		newMap.put('T', "-/");
+		newMap.put('t', "-/");
+		newMap.put('U', "..-/");
+		newMap.put('u', "..-/");
+		newMap.put('V', "...-/");
+		newMap.put('v', "...-/");
+		newMap.put('W', ".--/");
+		newMap.put('w', ".--/");
+		newMap.put('X', "-..-/");
+		newMap.put('x', "-..-/");
+		newMap.put('Y', "-.--/");
+		newMap.put('y', "-.--/");
+		newMap.put('Z', "--../");
+		newMap.put('z', "--../");
+		newMap.put(' ', "/");
 		
+		Scanner in = new Scanner(System.in);
 		
-		
-		
+		while (in.hasNextLine()){
+			
+			String lineString  = in.nextLine();
+			char[] charArray = lineString.toCharArray();
+			
+			for (int i = 0; i< lineString.length();i++){
+				
+				System.out.print(newMap.get(charArray[i]));
+				
+			}
+			System.out.println(" ");
+			
+		}
+		in.close();
 		
 	}
 
--- Spoj_str_danych/src/Algorithms/Hextobin.java
+++ Spoj_str_danych/src/Algorithms/Hextobin.java
@@ -1,54 +1,98 @@
+package Algorithms;
+
+import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.Map;
-
-public class Hextobin {
-
-
+import java.util.Scanner;
+ class Hextobin {
+	ArrayList <Integer> tab = new ArrayList <Integer>();
+	String binStr = null;
 	
-
+	public String toBinary(int decimal) {
 	
-	
-	
-	
-	public int toBinary(int decimal) {
-		String binStr = null;
-		int bin;
-		int i = 0;
-		int tab[] = new int[31];
-
 		while (decimal != 0) {
-
-			tab[i++] = decimal % 2;
+			tab.add (decimal % 2) ;
 			decimal /= 2;
-
 		}
+		
+		 if(tab.size()==1){
+			
+			binStr = "000";
 
-		for (int j = i - 1; j >= 0; j--) {
+			for(int j = tab.size()-1; j >= 0; j--){
+			
+				binStr += tab.get(j);
+			}
+		}
+		
+		
+		
+		else if(tab.size()==2){
+		
+			binStr = "00";
+			
+			for(int j = tab.size()-1; j >= 0; j--){
+				
+				binStr += tab.get(j);
+			}
+		}
+		
+		else if (tab.size()==3){
+			
+	binStr = "0";
+			
+	for(int j = tab.size()-1; j >= 0; j--){
+		
+		binStr += tab.get(j);
+	}}
+			
+			else if (tab.size()==4){
+
+		for (int k = tab.size()-1; k >= 0; k--) {
 
 			if (binStr == null) {
 
-				binStr = Integer.toString(tab[j]);
+				binStr = Integer.toString(tab.get(k));
 				continue;
 			}
-			binStr += Integer.toString(tab[j]);
+			binStr += Integer.toString(tab.get(k));
 
-		}
+		}}
 
-		return bin = Integer.parseInt(binStr);
+		return binStr;
 	}
 
 	public static void main(String[] args) {
 
-		HashMap<String, Integer> myMap = new HashMap <String, Integer>();
-		
-		myMap.put("1", 1);
-	
-		
+		HashMap<String, Integer> newMap = new HashMap<String, Integer>();
+
+		newMap.put("0", 0);
+		newMap.put("1", 1);
+		newMap.put("2", 2);
+		newMap.put("3", 3);
+		newMap.put("4", 4);
+		newMap.put("5", 5);
+		newMap.put("6", 6);
+		newMap.put("7", 7);
+		newMap.put("8", 8);
+		newMap.put("9", 9);
+		newMap.put("A", 10);
+		newMap.put("B", 11);
+		newMap.put("C", 12);
+		newMap.put("D", 13);
+		newMap.put("E", 14);
+		newMap.put("F", 15);
+
 		Hextobin sample = new Hextobin();
 
-		sample.toBinary(3);
+		Scanner x = new Scanner(System.in);
+		 String r = x.nextLine();
+		 x.close();
+		
+		
+	int decimal = newMap.get(r);
 
-		System.out.println(sample.toBinary(3));
+
+		System.out.println(sample.toBinary(decimal));
 
 	}
 
